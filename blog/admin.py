ad9aca1abcf1bb2e8743a215d5af2f04ff8c1ade
pangeranhabibi <pangeranhabibi162@gmail.com>
2025-09-10 11:27:32 +0700

    nambahin view dan template buat detail per blog post nya

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'created_date', 'author')
